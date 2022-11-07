e045166946d4b3dd00520e7bff1cb7c9ba596448
ramphy0x90 <ramphy11@hotmail.com>
2022-11-07 22:11:57 +0100

    Update credential controller, get credential

--- src/main/java/com/soteria/controllers/CredentialController.java
+++ src/main/java/com/soteria/controllers/CredentialController.java
@@ -1,10 +1,14 @@
 package com.soteria.controllers;
 
 import com.soteria.models.Credential;
+import com.soteria.models.User;
+import com.soteria.security.jwt.JwtTokenProvider;
 import com.soteria.services.CredentialService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.soteria.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -12,13 +16,34 @@ import java.util.List;
 @RequestMapping(path = "api/v1/credential")
 public class CredentialController {
     private final CredentialService credentialService;
+    private final UserService userService;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    public CredentialController(CredentialService credentialService) {
+    @Autowired
+    public CredentialController(CredentialService credentialService,
+                                JwtTokenProvider jwtTokenProvider,
+                                UserService userService) {
         this.credentialService = credentialService;
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.userService = userService;
     }
 
     @GetMapping(path = "/all")
-    public List<Credential> getCredentials() {
-        return credentialService.getCredentials(1L);
+    public List<Credential> getCredentials(@RequestHeader("Authorization") String token) {
+        String userName = jwtTokenProvider.getUserNameFromJwt(token);
+        User user = userService.getUser(userName);
+        Long userId = user.getId();
+
+        return credentialService.getCredentials(userId);
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Credential> getCredential(@RequestHeader("Authorization") String token,
+                                                    @PathVariable("id") Long credentialId) {
+        String userName = jwtTokenProvider.getUserNameFromJwt(token);
+        User user = userService.getUser(userName);
+        Long userId = user.getId();
+
+        return new ResponseEntity<>(credentialService.getCredential(userId, credentialId), HttpStatus.OK);
     }
 }
