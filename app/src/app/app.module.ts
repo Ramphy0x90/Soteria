908f779f7751db3c8746caf75b96691f835cb89f
Ramphy Aquino Nova <30904905+Ramphy0x90@users.noreply.github.com>
2022-11-09 11:36:18 +0100

    Modules update

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './containers/home/home.component';
+import { HomeAppComponent } from './containers/home-app/home-app.component';
+import { LoginComponent } from './containers/login/login.component';
+import { SignupComponent } from './containers/signup/signup.component';
+import { LogInComponent } from './components/user/log-in/log-in.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HomeAppComponent,
+    LoginComponent,
+    SignupComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
