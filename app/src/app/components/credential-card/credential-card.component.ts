c496c229da1c8f2aa6dcc3a351fc9198ea145a70
ramphy0x90 <ramphy11@hotmail.com>
2022-11-12 17:39:36 +0100

    Home app credential card

--- app/src/app/components/credential-card/credential-card.component.ts
+++ app/src/app/components/credential-card/credential-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Credential } from 'src/app/models/credential';
 
 @Component({
   selector: 'app-credential-card',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./credential-card.component.sass']
 })
 export class CredentialCardComponent implements OnInit {
+  @Input() credential!: Credential;
+  @Input() active: boolean = false;
+  icon!: string;
 
   constructor() { }
 
