5f28af3c15752773c285b205b02a751d73690639
ramphy0x90 <ramphy11@hotmail.com>
2022-11-12 17:35:08 +0100

    Authentication guard

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -4,11 +4,12 @@ import { HomeAppComponent } from './containers/home-app/home-app.component';
 import { HomeComponent } from './containers/home/home.component';
 import { LoginComponent } from './containers/login/login.component';
 import { SignupComponent } from './containers/signup/signup.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'home'},
   {path: 'home', component: HomeComponent},
-  {path: 'app', component: HomeAppComponent},
+  {path: 'app', component: HomeAppComponent, canActivate: [AuthGuard]},
   {path: 'login', component: LoginComponent},
   {path: 'signup', component: SignupComponent}
 ];
