52199d89da3d780ab73403b7826f296ec5d7434f
ramphy0x90 <ramphy11@hotmail.com>
2022-11-12 17:41:44 +0100

    Home app

--- app/src/app/containers/home-app/home-app.component.ts
+++ app/src/app/containers/home-app/home-app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Credential } from 'src/app/models/credential';
+import { CredentialService } from 'src/app/services/credential.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-home-app',
@@ -7,14 +9,32 @@ import { Credential } from 'src/app/models/credential';
   styleUrls: ['./home-app.component.sass']
 })
 export class HomeAppComponent implements OnInit {
+  loggedUser!: String | null | undefined;
   credentials: Credential[] = [
-    {id: null, entity: 1, userName: 'Ramphy0x90', password: 'password'},
-    {id: null, entity: 2, userName: '_ramphy_', password: 'password1'}
+    {id: 1, entity: 1, userName: 'Ramphy0x90', password: 'Ramphy123@'},
+    {id: 2, entity: 2, userName: '_ramphy_', password: 'password'},
+    {id: 3, entity: 3, userName: 'ramphy_an@outlook.com', password: 'helloWorld'}
   ];
 
-  constructor() { }
+  selectedCredential: Credential = this.credentials[0];
+
+  constructor(private userService: UserService, private credentialService: CredentialService) { }
 
   ngOnInit(): void {
+    this.credentialService.getCredentials().subscribe({
+      next: (data) => {
+        this.credentials = data;
+      }
+    })
+    this.loggedUser = this.userService.getLoggedUser()?.userName
+  }
+
+  selectCredential(credential: Credential): void {
+    this.selectedCredential = credential;
+  }
+
+  deleteCredential(): void {
+
   }
 
 }
