98f8acf60541964336a5a5afb6e255e4355db1dd
ramphy0x90 <ramphy11@hotmail.com>
2022-11-12 17:42:07 +0100

    Auhtenticator interceptor

---
+++ app/src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { UserService } from '../services/user.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private userService: UserService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    request = request.clone({
+      setHeaders: {
+        'Authorization': `Bearer ${this.userService.getLoggedUserToken()}`,
+      },
+    });
+
+    return next.handle(request);
+  }
+}
