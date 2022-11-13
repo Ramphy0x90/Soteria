a26d4b26c7c0a8985b0ce70154c7d72f3d93d80e
ramphy0x90 <ramphy11@hotmail.com>
2022-11-13 12:55:29 +0100

    Add credential form

--- app/src/app/components/credential-form/credential-form.component.ts
+++ app/src/app/components/credential-form/credential-form.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Credential } from 'src/app/models/credential';
+import { Entity } from 'src/app/models/entity';
+import { CredentialService } from 'src/app/services/credential.service';
+import { EntityService } from 'src/app/services/entity.service';
 
 @Component({
   selector: 'app-credential-form',
@@ -12,13 +15,53 @@ export class CredentialFormComponent implements OnInit {
   @Input() mode: number = 0;
 
   passwordVisible: boolean = false;
+  credentialForm: Credential = {
+    id: null,
+    entity: null,
+    entityId: 0,
+    userName: '',
+    password: ''
+  };
+  invalidCredentials: boolean = false;
+  errorMessage = '';
+  formSubmitted: boolean = false;
+  entities!: Entity[];
 
-  constructor() { }
+
+  constructor(private entityService: EntityService, private credentialService: CredentialService) { }
 
   ngOnInit(): void {
+    this.entityService.getEntities().subscribe({
+      next: (data) => {
+        this.entities = data;
+      }
+    });
+
+    if(this.mode == 2) {
+      this.credentialForm = {
+        id: this.credential.id,
+        entity: this.credential.entity,
+        entityId: this.credential.entity?.id,
+        userName: this.credential.userName,
+        password: this.credential.password
+      }
+    }
   }
 
   passwordVisibility(): void {
     this.passwordVisible = !this.passwordVisible;
+  }
+
+  onSubmit(): void {
+    let credential: Credential = this.credentialForm;
+
+    this.credentialService.addCredential(credential).subscribe({
+      next: (data) => {
+        console.log(data)
+      },
+      error: (err) => {
+        this.invalidCredentials = true;
+      }
+    });
   }
 }
