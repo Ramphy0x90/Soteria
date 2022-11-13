69b4042c7ae9a263cc89d6a7ee1d79ef096a3bc8
ramphy0x90 <ramphy11@hotmail.com>
2022-11-13 12:56:11 +0100

    Add credential service function

--- app/src/app/services/credential.service.ts
+++ app/src/app/services/credential.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { UserService } from './user.service';
+import { Credential } from 'src/app/models/credential';
 
 @Injectable({
   providedIn: 'root'
@@ -23,6 +23,12 @@ export class CredentialService {
   getCredential(id: number): Observable<any> {
     return this.httpClient.get(
       `${environment.server}/credential/${id}`, this.httpOptions
+    );
+  }
+
+  addCredential(credential: Credential): Observable<any> {
+    return this.httpClient.post(
+      `${environment.server}/credential/add`, credential, this.httpOptions
     );
   }
 }
